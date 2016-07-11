3ef74d7b471596492d2f2cfba97579a526c7aacd
MQ <qucool@gmail.com>
2016-07-11 10:00:26 +0800

    Support multiple types in monitor manager

--- apps/aspirin_monitor/lib/aspirin/monitor_manager.ex
+++ apps/aspirin_monitor/lib/aspirin/monitor_manager.ex
@@ -1,5 +1,13 @@
 defmodule Aspirin.MonitorManager do
   use GenServer
+  alias Aspirin.MonitorEvent
+  alias Aspirin.PortMonitor
+  alias Aspirin.PingMonitor
+  import Ecto.Query, only: [from: 2]
+
+  @types ~w(port ping)
+
+  ## API
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, :ok, opts)
@@ -14,12 +22,20 @@ defmodule Aspirin.MonitorManager do
   end
 
   def add_port_monitor(pid, id, ip, port) do
-    GenServer.call(pid, {:add_monitor, :port, id, ip, port})
+    opts = %{pid: pid, id: id, ip: ip, port: port, type: "port"}
+    GenServer.call(pid, {:add_monitor, opts})
+  end
+
+  def add_ping_monitor(pid, id, ip) do
+    opts = %{pid: pid, id: id, ip: ip, type: "ping"}
+    GenServer.call(pid, {:add_monitor, opts})
   end
 
   def remove_monitor(pid, id) do
     GenServer.call(pid, {:remove_monitor, id})
   end
+
+  ## Callbacks
 
   def init(:ok) do
     {:ok, initial_state}
@@ -30,55 +46,72 @@ defmodule Aspirin.MonitorManager do
   end
 
   def handle_call(:sync_repo, _from, %{monitors: _monitors} = state) do
-    import Ecto.Query, only: [from: 2]
-    events = from(m in Aspirin.MonitorEvent, where: m.enabled == "true")
-    |> Aspirin.Repo.all
+    events = from(m in MonitorEvent, where: m.enabled == "true")
+             |> Aspirin.Repo.all
 
-    new = Enum.reduce(events, stop_all(state), fn(monitor, acc) ->
-      {:ok, acc} = add_monitor(:port, monitor.id, monitor.addr, monitor.port, acc)
-      acc
+    new = Enum.reduce(events, stop_all(state), fn(mon, acc) ->
+      acc |> add_monitor(mon)
     end)
     {:reply, :ok, new}
   end
 
-  def handle_call({:add_monitor, :port, id, ip, port}, _from, state) do
-    {:ok, state} = add_monitor(:port, id, ip, port, state)
-    {:reply, :ok, state}
+  def handle_call({:add_monitor, %{type: type} = monitor}, _from, state)
+      when type in @types do
+    {:reply, :ok, state |> add_monitor(monitor)}
   end
 
   def handle_call({:remove_monitor, id}, _from, %{monitors: monitors} = state)
-  when is_integer(id) do
+      when is_integer(id) do
     case monitors do
-      %{^id => %{pid: pid}} ->
-        :ok = Aspirin.PortMonitor.stop(pid)
+      %{^id => %{pid: pid, type: type}} ->
+        if type == "port" do
+          :ok = PortMonitor.stop(pid)
+        end
         {:reply, :ok, %{monitors: Map.delete(monitors, id)}}
       _ ->
         {:reply, :ok, state}
     end
   end
 
+  ## Helpers
+
   defp stop_all(%{monitors: monitors}) do
     monitors |> Map.values |> Enum.each(fn(%{pid: pid}) ->
-      :ok = Aspirin.PortMonitor.stop(pid)
+      :ok = PortMonitor.stop(pid)
     end)
     initial_state
   end
 
-  defp add_monitor(:port, id, ip, port, %{monitors: monitors} = state)
-  when is_integer(id) and is_binary(ip) and is_integer(port) do
+  @attrs [:id, :ip, :port, :type]
+
+  defp add_monitor(state, %MonitorEvent{addr: ip} = monitor)
+      when is_map(state) do
+    new_mon = monitor
+              |> Map.from_struct
+              |> Map.put_new(:ip, ip)
+              |> Map.take(@attrs)
+    add_monitor(state, new_mon)
+  end
+
+  defp add_monitor(%{monitors: monitors} = state,
+       %{id: id, ip: ip, type: type} = monitor)
+       when is_integer(id) and is_binary(ip) and type in @types do
     case monitors do
       %{^id => _monitor} ->
-        {:ok, state}
+        state
       _ ->
-        {:ok, pid} = Aspirin.PortMonitor.start_link(ip, port)
-        :ok = Aspirin.PortMonitor.start_monitor(pid)
-        new = put_in(state, [:monitors, id], %{id: id, ip: ip, port: port, pid: pid})
-        IO.puts("[INFO] new state: " <> inspect(new))
-        {:ok, new}
+        {:ok, pid} = start_monitor(monitor)
+        put_in(state, [:monitors, id], Map.put(monitor, :pid, pid))
     end
   end
 
-  defp initial_state do
-    %{monitors: %{}}
+  defp start_monitor(%{ip: ip, port: port, type: "port"}) do
+    {:ok, pid} = PortMonitor.start_link(ip, port)
+    :ok = PortMonitor.start_monitor(pid)
+    {:ok, pid}
   end
+
+  defp start_monitor(%{ip: ip, type: "ping"}), do: PingMonitor.start_link(ip)
+
+  defp initial_state, do: %{monitors: %{}}
 end
